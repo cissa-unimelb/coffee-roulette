fa2fb108ede53952194d83c2dcaf621c8ce80b9a
thebowenfeng <857514.leofeng@gmail.com>
2023-08-02 01:11:09 +1000

    finalized cron functionality

--- cron_scheduler.py
+++ cron_scheduler.py
@@ -1,7 +1,7 @@
 from crontab import CronTab
 
 cron = CronTab(user='root')
-job = cron.new(command='/usr/bin/python3 /mnt/d/Projects/CoffeeRoulette/cron_test.py')
+job = cron.new(command='/usr/bin/python /home/pi/coffee-roulette/main.py')
 job.day.on(1, 15)
 job.hour.on(0)
 job.minute.on(0)
